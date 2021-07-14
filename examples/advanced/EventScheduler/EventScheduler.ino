4a614512ec50fbb759af8c7c9285b095076a032d
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-07-14 12:42:08 +0200

    Add files via upload

--- examples/advanced/EventScheduler/EventScheduler.ino
+++ examples/advanced/EventScheduler/EventScheduler.ino
@@ -1,4 +1,4 @@
- /*
+/*
   Created:     24/03/2020
   Author:      Tolentino Cotesta <cotestatnt@yahoo.com>
   Advance example with OTA support and "event scheduler".
@@ -21,43 +21,39 @@
 #include <AsyncTelegram2.h>
 #include <FS.h>
 #ifdef ESP8266
-  #include <ESP8266WiFi.h>
-  #include <LittleFS.h>
-  #define FILESYSTEM LittleFS
-  BearSSL::WiFiClientSecure client;
-  BearSSL::Session   session;
-  BearSSL::X509List  certificate(telegram_cert);
+#include <ESP8266WiFi.h>
+#include <LittleFS.h>
+#define FILESYSTEM LittleFS
+BearSSL::WiFiClientSecure client;
+BearSSL::Session   session;
+BearSSL::X509List  certificate(telegram_cert);
 
 #elif defined(ESP32)
-  #include <WiFi.h>
-  #include <SPIFFS.h>
-  #define FILESYSTEM SPIFFS
-  #if USE_CLIENTSSL
-    #include <WiFiClient.h>
-    #include <SSLClient.h>
-    #include "certificates.h"
-    WiFiClient base_client;
-    SSLClient client(base_client, TAs, (size_t)TAs_NUM, A0, 1, SSLClient::SSL_ERROR);
-  #else
-    #include <WiFiClientSecure.h>
-    WiFiClientSecure client;
-  #endif
+#include <WiFi.h>
+#include <SPIFFS.h>
+#define FILESYSTEM SPIFFS
+#if USE_CLIENTSSL
+#include <WiFiClient.h>
+#include <SSLClient.h>
+#include "certificates.h"
+WiFiClient base_client;
+SSLClient client(base_client, TAs, (size_t)TAs_NUM, A0, 1, SSLClient::SSL_ERROR);
+#else
+#include <WiFiClientSecure.h>
+WiFiClientSecure client;
+#endif
 #endif
 
 AsyncTelegram2 myBot(client);
 
-const char* ssid  =  "xxxxxxxxx";     // SSID WiFi network
-const char* pass  =  "xxxxxxxxx";     // Password  WiFi network
-const char* token =  "xxxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";  // Telegram token
+const char* ssid  =  "xxxxxxxxxx";     // SSID WiFi network
+const char* pass  =  "xxxxxxxxxx";     // Password  WiFi network
+const char* token =  "xxxxxxxxxxxx ";  // Telegram token
 
 
 // Check the userid with the help of bot @JsonDumpBot or @getidsbot (work also with groups)
 // https://t.me/JsonDumpBot  or  https://t.me/getidsbot
 int64_t userid = 1234567890;
-
-ReplyKeyboard weekdaysKbd;
-InlineKeyboard scheduleKbd;
-ReplyKeyboard myKbd;
 
 const char* update_password = "update";
 const char* firmware_version = __TIME__;
@@ -96,20 +92,17 @@ void setup() {
   loadConfigFile();
 
 #ifdef ESP8266
-  // We have to handle reboot manually after sync with TG server
-  ESPhttpUpdate.rebootOnUpdate(false);
-  // Sync time with NTP (don't skip this step, secure connection needs synced clock)
+    // Sync time with NTP (don't skip this step, secure connection needs synced clock)
   configTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
   // Set certficate, session and some other base client properies
   client.setSession(&session);
   client.setTrustAnchors(&certificate);
   client.setBufferSizes(1024, 1024);
 #elif defined(ESP32)
-  httpUpdate.rebootOnUpdate(false);
   configTzTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
-  #if USE_CLIENTSSL == false
-    client.setCACert(telegram_cert);
-  #endif
+#if USE_CLIENTSSL == false
+  client.setCACert(telegram_cert);
+#endif
 #endif
 
   // Now is possible start Telegram connection
@@ -119,10 +112,6 @@ void setup() {
   // Check if all things are ok
   Serial.print("\nTest Telegram connection... ");
   myBot.begin() ? Serial.println("OK") : Serial.println("NOK");
-
-  getSchedulerKeyboard(&scheduleKbd);
-  getWeekdayKeyboard(&weekdaysKbd);
-  getCommandKeyboard(&myKbd);
 
   const char* botName = myBot.getBotName();
   Serial.printf("Nome del bot: @%s", botName);
@@ -135,7 +124,7 @@ void setup() {
 
 
 void loop() {
-  if(do_restart)
+  if (do_restart)
     doRestart();
 
   printHeapStats();
@@ -143,23 +132,23 @@ void loop() {
   // get updated timedate and check if there is any active event
   time_t epoch = time(nullptr);
   tm now = *gmtime(&epoch);
-  uint32_t startOfDay = mktime(&now) - now.tm_hour*3600UL  -  now.tm_min*60UL - now.tm_sec;
+  uint32_t startOfDay = mktime(&now) - now.tm_hour * 3600UL  -  now.tm_min * 60UL - now.tm_sec;
 
-  for(uint8_t i=0; i< lastEvent; i++){
+  for (uint8_t i = 0; i < lastEvent; i++) {
     // For each event stored we evaluate whether it is necessary to activate
     if (startOfDay + events[i].start == (uint32_t) epoch
         && bitRead(events[i].days, now.tm_wday) )
-        //&& events[i].setpoint > actual_temperature
+      //&& events[i].setpoint > actual_temperature
     {
       events[i].active = true;  // Now we can use this as trigger for any need
       delay(500);
-      Serial.printf("\nEvent n. %d actvivated", i+1);
+      Serial.printf("\nEvent n. %d actvivated", i + 1);
     }
 
     if (startOfDay + events[i].stop == (uint32_t) epoch) {
       events[i].active = false;
       delay(500);
-      Serial.printf("\nEvent n. %d deactvivated", i+1);
+      Serial.printf("\nEvent n. %d deactvivated", i + 1);
     }
   }
 
@@ -177,48 +166,51 @@ void loop() {
     bool msgParsed =  updateHandler(msg, myBot);
 
     // maybe is for events scheduler handler?
-    if(! msgParsed)
+    if (! msgParsed)
       msgParsed = schedulerHandler(msg, myBot);
 
     // none of the above, process the message received for remaining cases
-    if (! msgParsed){
+    if (! msgParsed) {
       String txtMessage = msg.text;
       Serial.print("\nMessagge: ");
       Serial.println(txtMessage);
 
       // Send back to user current time
       if (txtMessage.equalsIgnoreCase("/time")) {
-          time_t epoch = time(nullptr);
-          now = *localtime(&epoch);
-          char buffer [40];
-          strftime (buffer, 40,"%c", &now);
-          myBot.sendMessage(msg, buffer);
+        time_t epoch = time(nullptr);
+        now = *localtime(&epoch);
+        char buffer [40];
+        strftime (buffer, 40, "%c", &now);
+        myBot.sendMessage(msg, buffer);
       }
       // Inform the user about actual firmware version
       else if (txtMessage.equalsIgnoreCase("/version")) {
-          char fw_info[30];
-          snprintf(fw_info, 30, "Firmware version: %s", firmware_version);
-          myBot.sendMessage(msg, fw_info);
+        char fw_info[30];
+        snprintf(fw_info, 30, "Firmware version: %s", firmware_version);
+        myBot.sendMessage(msg, fw_info);
       }
       // User request a restart
       else if (txtMessage.equalsIgnoreCase("/restart")) {
-          myBot.sendMessage(msg, "Going to restart MCU...");
-          do_restart = true;
+        myBot.sendMessage(msg, "Going to restart MCU...");
+        do_restart = true;
       }
       // User request a list of supported commands
       else if (txtMessage.equalsIgnoreCase("/help")) {
-          myBot.sendMessage(msg,"To update firmware, upload compiled file ########.bin \n"
-                                "System:\n/restart for restart MCU\n"
-                                "/version for print the current firmware version\n"
-                                "/time for local system timedate\n\nScheduler:\n"
-                                "/clear delete all scheduled events \n\n"
-                                "/list print all events in memory\n"
-                                "/edit to edit a specific event\n"
-                                "/add to add new event to list", myKbd);
+        // Create ReplyKeyboard keyboard and send with message
+        ReplyKeyboard myKbd;
+        getCommandKeyboard(&myKbd);
+        myBot.sendMessage(msg, "To update firmware, upload compiled file ########.bin \n"
+                          "System:\n/restart for restart MCU\n"
+                          "/version for print the current firmware version\n"
+                          "/time for local system timedate\n\nScheduler:\n"
+                          "/clear delete all scheduled events \n\n"
+                          "/list print all events in memory\n"
+                          "/edit to edit a specific event\n"
+                          "/add to add new event to list", myKbd);
       }
       // None of the above
       else {
-          myBot.sendMessage(msg, "Command not supported. Type /help for complete list");
+        myBot.sendMessage(msg, "Command not supported. Type /help for complete list");
       }
     }
 
@@ -226,7 +218,7 @@ void loop() {
 }
 
 
-bool loadConfigFile(){
+bool loadConfigFile() {
   // Open file for reading
   File file = FILESYSTEM.open(filename, "r");
   if (!file) {
@@ -241,10 +233,10 @@ bool loadConfigFile(){
 
   serializeJsonPretty(doc, Serial);
 
-  for(uint8_t i=0; i<MAX_EVENTS; i++){
+  for (uint8_t i = 0; i < MAX_EVENTS; i++) {
     String evt_name = "event";
     evt_name += i + 1;
-    if(doc[evt_name]) {
+    if (doc[evt_name]) {
       events[i].start = doc[evt_name]["start"];
       events[i].stop = doc[evt_name]["stop"];
       events[i].days = doc[evt_name]["days"];
@@ -258,7 +250,7 @@ bool loadConfigFile(){
 }
 
 
-void saveConfigFile(){
+void saveConfigFile() {
   // Open file for writing
   File file = FILESYSTEM.open(filename, "w");
   if (file)
@@ -270,9 +262,9 @@ void saveConfigFile(){
   StaticJsonDocument<1024> doc;
   JsonObject root = doc.to<JsonObject>();
 
-  for(uint8_t i=0; i<lastEvent; i++){
+  for (uint8_t i = 0; i < lastEvent; i++) {
     String evt_name = "event";
-    evt_name += i+1;
+    evt_name += i + 1;
     JsonObject evt = root.createNestedObject(evt_name);
     evt["start"] = events[i].start;
     evt["stop"] = events[i].stop;
@@ -289,7 +281,7 @@ void saveConfigFile(){
   file.close();
 }
 
-void doRestart(){
+void doRestart() {
   do_restart = false;
   // Wait until bot synced with telegram to prevent cyclic reboot
   while (! myBot.noNewMessage()) {
@@ -310,13 +302,13 @@ void printHeapStats() {
 #ifdef ESP32
     //heap_caps_print_heap_info(MALLOC_CAP_DEFAULT);
     Serial.printf("%02d:%02d:%02d - Total free: %6d - Max block: %6d\n",
-      tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, heap_caps_get_free_size(0), heap_caps_get_largest_free_block(0) );
+                  tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, heap_caps_get_free_size(0), heap_caps_get_largest_free_block(0) );
 #elif defined(ESP8266)
     uint32_t free;
     uint16_t max;
     ESP.getHeapStats(&free, &max, nullptr);
     Serial.printf("%02d:%02d:%02d - Total free: %5d - Max block: %5d\n",
-      tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, free, max);
+                  tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, free, max);
 #endif
   }
 }
--- examples/advanced/EventScheduler/fw_updater.h
+++ examples/advanced/EventScheduler/fw_updater.h
@@ -5,110 +5,128 @@ extern const char* update_password;
 extern void doRestart();
 
 #ifdef ESP8266
-    #include <ESP8266httpUpdate.h>
+#include <ESP8266httpUpdate.h>
+#define UPDATER ESPhttpUpdate
 #elif defined(ESP32)
-    #include <HTTPClient.h>
-    #include <HTTPUpdate.h>
+#include <HTTPUpdate.h>
+#define UPDATER httpUpdate
 #endif
 
+
 bool updateHandler(TBMessage &theMsg, AsyncTelegram2 &theBot) {
-    bool parsed = false;
-    static String fw_path;
-    switch (theMsg.messageType) {
-        // Type of message "document"
-        case MessageDocument: {
-            // Store in memory link to the firmware file
-            fw_path = theMsg.document.file_path;
-            if (theMsg.document.file_exists) {
-                // Check file extension of received document (firmware must be .bin)
-                if( theMsg.document.file_path.endsWith(".bin")) {
-                    char report [128];
-                    snprintf(report, 128, "Start firmware update\nFile name: %s\nFile size: %d",
-                        theMsg.document.file_name, theMsg.document.file_size);
-                    // Inform user and query for flash confirmation with password
-                    theBot.sendMessage(theMsg, report, "");
 
-                   // Force reply don't work with web version of Telegram Client (use Telegram Desktop or mobile app)
-                    theMsg.force_reply = true;
-                    theBot.sendMessage(theMsg, "Please, reply to this message with the right password", "");  
-                }
-            }
-            else {
-                theBot.sendMessage(theMsg, "File is unavailable. Maybe size limit 20MB was reached or file deleted");
-            }
-            parsed = true;
-            break;
+  bool msgParsed = false;             // A variable to inform caller that msg is for this function
+  static String fw_path;              // The remote firmware path (passed from Telegram server)
+  static bool updateRequest = false;  // A variable to handle only MessageReply related to firmware update
+
+  // Check message type
+  switch (theMsg.messageType) {
+
+    // Here will be evalueted only MessageDocument and MessageReply type
+    default:
+      break;
+
+    // The massage received if "document" type (binary file)
+    case MessageDocument: {
+
+        // Store in memory link to the firmware file
+        fw_path = theMsg.document.file_path;
+        if (theMsg.document.file_exists) {
+
+          // Check file extension of received document (firmware must be .bin)
+          if ( theMsg.document.file_path.endsWith(".bin")) {
+            char report [128];
+            snprintf(report, 128, "Start firmware update\nFile name: %s\nFile size: %d",
+                     theMsg.document.file_name, theMsg.document.file_size);
+
+            // Inform user and query for flash confirmation with password
+            theBot.sendMessage(theMsg, report, "");
+
+            // Force reply don't work with web version of Telegram Client (use Telegram Desktop or mobile app)
+            theMsg.force_reply = true;
+            theBot.sendMessage(theMsg, "Please, reply to this message with the right password", "");
+            updateRequest = true;
+          }
         }
+        else {
+          theBot.sendMessage(theMsg, "File is unavailable. Maybe size limit 20MB was reached or file deleted");
+        }
+        msgParsed = true;
+        break;
+      }
 
-        // This is a reply message, maybe the user reply with right password?
-        case MessageReply: {
-            // User has confirmed flash start with right password
-            if (theMsg.text.equalsIgnoreCase(update_password)) {
-                theBot.sendMessage(theMsg, "Start flashing... please wait (~30/60s)");
-                Serial.print("Firmware path: ");
-                Serial.println(fw_path);
-                if(fw_path.length() == 0)
-                    break;
-                
-                #ifdef ESP8266              
-                // onProgress handling is missing with ESP32 library
-                ESPhttpUpdate.onProgress([](int cur, int total){
-                    static uint32_t sendT;
-                    if(millis() - sendT > 1000){
-                        sendT = millis();
-                        Serial.printf("Updating %d of %d bytes...\n", cur, total);
-                    }
-                });
-                t_httpUpdate_return ret = ESPhttpUpdate.update(client, fw_path, firmware_version);
-                client.stop();
-                #elif defined(ESP32)
-                WiFiClientSecure updateClient;
-                updateClient.setInsecure();
-                updateClient.setTimeout(12); // Increase timeout
-                t_httpUpdate_return ret = httpUpdate.update(updateClient, fw_path, firmware_version);
-                updateClient.stop();
-                #endif
-                
-               
-                fw_path = "";
+    // This is a reply message, maybe the user reply with password?
+    case MessageReply: {
+
+        // User has confirmed flash start
+        if (updateRequest) {
+
+          // Wrong password
+          if (!theMsg.text.equalsIgnoreCase(update_password)) {
+            theBot.sendMessage(theMsg, "You have entered wrong password");
+            fw_path = "";
+            msgParsed = true;
+            break;
+          }
+
+          // Check if previous message has a valid firmware remote path
+          if (fw_path.length() != 0) {
+            updateRequest = false;
+            theBot.sendMessage(theMsg, "Start flashing... please wait (~30/60s)");
+            Serial.printf("Firmware path: %s\n", fw_path.c_str());
+          }
+          else
+            break;
+
+          // onProgress handling is missing with ESP32 library
+#ifdef ESP8266
+          UPDATER.onProgress([](int cur, int total) {
+            static uint32_t sendT;
+            if (millis() - sendT > 1000) {
+              sendT = millis();
+              Serial.printf("Updating %d of %d bytes...\n", cur, total);
+            }
+          });
+#endif
+
+          // Start remote firmware update
+          WiFiClientSecure updateClient;
+          updateClient.setInsecure();         
+
+          // We have to handle reboot manually after sync with TG server
+          UPDATER.rebootOnUpdate(false);
+          t_httpUpdate_return ret = UPDATER.update(updateClient, fw_path, firmware_version);
+          updateClient.stop();
+          fw_path = "";
+
+          // Evaluate the result
+          switch (ret) {
+            default: {
                 char report [128];
-                switch (ret) {
-                    default:
-                    #ifdef ESP8266                        
-                        snprintf(report, 128, "HTTP_UPDATE_FAILED Error (%d): %s",
-                                ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-                        theBot.sendMessage(theMsg, report);
-                    #elif defined(ESP32)
-                        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
-                    #endif
-                        break;
+                snprintf(report, 128,
+                         "HTTP_UPDATE_FAILED Error (%d): %s",
+                         UPDATER.getLastError(),
+                         UPDATER.getLastErrorString().c_str());
+                theBot.sendMessage(theMsg, report);
+                break;
+              }
 
-                    case HTTP_UPDATE_NO_UPDATES:
-                        theBot.sendMessage(theMsg, "HTTP_UPDATE_NO_UPDATES");
-                        break;
+            case HTTP_UPDATE_NO_UPDATES:
+              theBot.sendMessage(theMsg, "HTTP_UPDATE_NO_UPDATES");
+              break;
 
-                    case HTTP_UPDATE_OK:
-                        theBot.begin();
-                        String infoStr = "UPDATE OK.\nRestarting in few seconds...";
-                        theBot.sendMessage(theMsg, infoStr);
-                        delay(100);
-                        // Wait until bot synced with telegram to prevent cyclic reboot
-                        doRestart();
-                        break;
-                }
-            }
-            // Wrong password
-            else{
-                theBot.sendMessage(theMsg, "You have entered wrong password");
-                fw_path = "";
-            }
-            parsed = true;
-            break;
+            case HTTP_UPDATE_OK:
+              theBot.begin();
+              String infoStr = "UPDATE OK.\nRestarting in few seconds...";
+              theBot.sendMessage(theMsg, infoStr);
+              delay(100);
+              // Wait until bot synced with telegram to prevent cyclic reboot
+              doRestart();
+              break;
+          }
         }
+      }
+  }
 
-        default:
-            break;
-    }
-
-    return parsed;
+  return msgParsed;
 }
